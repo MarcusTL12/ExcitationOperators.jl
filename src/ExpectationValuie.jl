9c44897d0fed44e34b3930b8c394a1c79aecf57a
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-08-25 10:31:34 +0200

    Fixed (hopefully) issue with multiplying term with colliding sum_indices

--- src/ExpectationValuie.jl
+++ src/ExpectationValuie.jl
@@ -24,11 +24,14 @@ function exval(t::CompositeTerm{T}) where {T<:Number}
         if length(t.operators) == 1
             o = first(t.operators)
             if o.p.o == occ || o.q.o == occ
-                nonop_part * 2δ(o.p, o.q)
+                summation(get_nosum(nonop_part) * 2δ(o.p, o.q),
+                    nonop_part.sum_inds)
             else
                 np = make_occ(o.p)
                 nq = make_occ(o.q)
-                exchange_index(nonop_part, [o.p => np, o.q => nq]) * 2δ(np, nq)
+                nonop_part = exchange_index(nonop_part, [o.p => np, o.q => nq])
+                summation(get_nosum(nonop_part) * 2δ(np, nq),
+                    nonop_part.sum_inds)
             end
         else
             i = findfirst(x -> x.p.o == vir || x.q.o == vir, t.operators)
@@ -37,15 +40,21 @@ function exval(t::CompositeTerm{T}) where {T<:Number}
                     i -= 1
                 end
 
-                exval(nonop_part * comm(
-                    CompositeTerm(t.operators[1:i]),
-                    CompositeTerm(t.operators[i+1:end])
-                ))
+                exval(summation(get_nosum(nonop_part) * comm(
+                        CompositeTerm(t.operators[1:i]),
+                        CompositeTerm(t.operators[i+1:end])
+                    ), nonop_part.sum_inds))
             elseif first(t.operators).q.o == occ
                 exval(CompositeTerm(first(t.operators))) *
-                exval(nonop_part * CompositeTerm(t.operators[2:end]))
+                exval(summation(
+                    get_nosum(nonop_part) * CompositeTerm(t.operators[2:end]),
+                    nonop_part.sum_inds
+                ))
             elseif last(t.operators).p.o == occ
-                exval(nonop_part * CompositeTerm(t.operators[1:end-1])) *
+                exval(summation(
+                    get_nosum(nonop_part) * CompositeTerm(t.operators[1:end-1]),
+                    nonop_part.sum_inds
+                )) *
                 exval(CompositeTerm(last(t.operators)))
             else
                 o = first(t.operators)
